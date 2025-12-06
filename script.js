de723d816a4c08d0176fb6693926add2ce827674
Rohit <192195109+rohitkay@users.noreply.github.com>
2025-12-06 19:54:30 +1100

    Create random number and if statements

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+console.log("Hello World");
+
+function getComputerChoice(){
+    let randomNumber = Math.random() * 10;
+    console.log(randomNumber);
+    if (randomNumber <= 33.33){
+        return "rock";
+    } 
+    else if (randomNumber <= 66.66){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
